0c71967be52ab8fe038e0a9bd96579260edcdad3
Rafael Reis <rafaelfc.ads@gmail.com>
2017-09-07 20:42:11 -0300

    add cors configuration

--- src/app/app.service.ts
+++ src/app/app.service.ts
@@ -9,8 +9,15 @@ export class AppService {
   constructor(private http: Http) { }
 
   getToggles(token) {
+
+    const headers = new Headers();
+    headers.append('Access-Control-Allow-Headers', 'Content-Type');
+    headers.append('Access-Control-Allow-Methods', 'GET');
+    headers.append('Access-Control-Allow-Origin', 'https://toggle-manager-api.herokuapp.com');
+    headers.append('Access-Control-Allow-Credentials', 'true');
+
     return this.http
-      .get('https://toggle-manager-api.herokuapp.com/api/users/59abcf98734d1d25a0f5dfba?access_token=' + token)
+      .get('https://toggle-manager-api.herokuapp.com/api/users/59abcf98734d1d25a0f5dfba?access_token=' + token, {headers: headers})
       .toPromise()
       .then(user => user.json())
       .catch(error => Promise.reject(error.message || error));
